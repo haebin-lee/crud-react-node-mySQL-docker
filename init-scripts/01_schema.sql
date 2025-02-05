387ac2b278338ffd1c63e758c6b4676a3a3352b3
haebin.lee <luckylucy0717@gmail.com>
2025-02-04 21:36:56 -0800

    feat: containerization

---
+++ init-scripts/01_schema.sql
@@ -0,0 +1,13 @@
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '';
+
+CREATE TABLE IF NOT EXISTS books (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL, 
+  description VARCHAR(1023) NOT NULL, 
+  cover VARCHAR(1023) NULL, 
+  price INT NOT NULL
+);
+
+-- Add some test data
+INSERT INTO books (title, description, price, cover) VALUES
+    ('Test Book', 'Test Description', 29, 'cover.jpg');
